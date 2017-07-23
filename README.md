a052348036063589b2263ffafc9790475feec5d4
Justin Brush <jbrush@pugetsound.edu>
2017-07-22 18:41:04 -0700

    Adds functions to a new basic functions file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # quick-bash
-Quick bash commands I have come to use in most dev environments
+
+After working in a bunch of different machines (local, vm, ec2-instance, etc.), I have cultivated a handful of universal commands that I find helpful in any environment.
+
+### How to use
+
+Add any commands you like to your .bash_profile or other bash files (I primarily work with .bash_profile.). You can copy and paste the functions into the bash profile of your choosing. Once you run 'source <bash_file>', you'll have access to these functions. Enjoy!
---
+++ basic.sh
@@ -0,0 +1,26 @@
+################################################################################
+#                               Basic Functions                                #
+################################################################################
+
+# These commands provide quick access to / editing of your .bash_profile
+
+# Reload .bash_profile via the 'source' command
+reload () {
+  echo Reloading .bash_profile...
+	source ~/.bash_profile
+	echo .bash_profile reloaded.
+}
+
+# Print .bash_profile
+show () {
+  cat ~/.bash_profile
+}
+
+# Edit .bash_profile using vi
+edit () {
+  vi ~/.bash_profile
+}
+
+# End of basic functions
+################################################################################
+################################################################################
